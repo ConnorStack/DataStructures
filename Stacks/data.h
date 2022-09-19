6972d7014b53c63e5a572ae2762b8a3dc1a2ddc6
Connor Stack <60162579+ConnorStack@users.noreply.github.com>
2022-09-18 22:23:51 -0500

    Create data.h

---
+++ Stacks/data.h
@@ -0,0 +1,20 @@
+/*
+ * COSC 2436
+ * Stacks Assignment
+ * Do not modify this comment
+ * 
+ * DO NOT MODIFY THIS FILE
+ */
+
+#ifndef STACKS_DATA_H
+#define STACKS_DATA_H
+
+#include <string>
+using std::string;
+
+struct Data {
+    int id;
+    string information;
+};
+
+#endif /* STACKS_DATA_H */
