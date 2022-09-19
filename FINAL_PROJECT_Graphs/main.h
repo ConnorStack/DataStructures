53204eb856305d7912e935d2d420fc8b8047f4b0
Connor Stack <60162579+ConnorStack@users.noreply.github.com>
2022-09-18 22:41:48 -0500

    Create main.h

---
+++ FINAL_PROJECT_Graphs/main.h
@@ -0,0 +1,24 @@
+/*
+ * File: main.h
+ * Author: Connor Stack
+ */
+#ifndef MAIN_H
+#define MAIN_H
+
+#define QUARTER_ALPHABET 7
+#define HALF_ALPHABET 13
+#define HALFSIZE 15
+#define SIZE 30
+#define DOUBLESIZE 52
+#define SIZE_ALPHABET 26
+#define NUMBERSET 50
+#define RANDSET 18
+
+#include "graph.h"
+#include <iostream>     
+#include <stdlib.h>     
+#include <time.h>       
+#include <string>      
+
+
+#endif //MAIN_H
