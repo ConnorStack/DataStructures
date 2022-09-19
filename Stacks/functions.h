f306d4d1dd55e7b1071f4a5c253ce22609d3016d
Connor Stack <60162579+ConnorStack@users.noreply.github.com>
2022-09-18 22:23:17 -0500

    Create functions.h

---
+++ Stacks/functions.h
@@ -0,0 +1,18 @@
+/*
+ * Functions.h
+ * Functions.cpp is made for creating random strings.
+ */
+
+#ifndef STACKS_FUNCTIONS_H
+#define STACKS_FUNCTIONS_H
+
+#define MAXSTRSIZE 8
+
+#include <stdlib.h>   
+#include <time.h>    
+#include <string>     
+
+void rand_string(std::string*);
+bool inputIntOnly(char *);
+
+#endif /* STACKS_FUNCTIONS_H */
