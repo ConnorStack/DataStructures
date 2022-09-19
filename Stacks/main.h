89755e1ffd37405a0ae401d55c53dcbe87e0db0d
Connor Stack <60162579+ConnorStack@users.noreply.github.com>
2022-09-18 22:22:15 -0500

    Create main.h

---
+++ Stacks/main.h
@@ -0,0 +1,23 @@
+/*
+ * File: main.h
+ */
+
+#ifndef STACKS_MAIN_H
+#define STACKS_MAIN_H
+#define MULTIPLIER 1.5 
+#define RANDOPTION 4
+#define LOOPMULTIPLIER 4
+#define BIGMULTIPLIER 4
+#define ARGCSET 1
+
+#include <stdlib.h>   
+#include <time.h>     
+#include <iostream>  
+#include <string>    
+
+#include "functions.h"
+#include "stack.h"
+using std::cout;
+using std::endl;
+
+#endif /* STACKS_MAIN_H */
